8c4726166ff37fe320a490818e58af849fefc86c
Daniel Fliegauf <daniel.fliegauf@sap.com>
2014-06-16 17:01:43 -0700

    Check Fivelet instead of Triplet
    Bugfixes and Fivelet implementation.
    timediff gives out bmp but not really accurate :(

--- nodeServer.js
+++ nodeServer.js
@@ -1,55 +1,65 @@
 var http = require('http');
 var fs = require('fs');
 
-http.createServer(function (req, res) {
-    res.writeHead(200, {
-        'Content-Type': 'text/plain'
-    });
-
-    var measurements = [];
-
-    var peakFinder = function (triplet) {
-        if ((triplet !== Array) && (triplet.length !== 3))
+http.createServer(function(req, res) {
+	res.writeHead(200, {
+		'Content-Type': 'text/plain'
+	});
+    
+	var measurements = [];
+    
+    var peakFinder = function(fivelet){
+        if(!(fivelet instanceof Array) || fivelet.length != 5 || fivelet.length != '5'){
+            //res.write("fiveletlength"+fivelet.length+"\n");
             return 0;
-        var first = triplet[0].frequency;
-        var second = triplet[1].frequency;
-        var third = triplet[2].frequency;
-
-        if ((second > first) && (second > third)) {
-            return triplet[1].timestamp;
+        }
+        var first = fivelet[0].frequency;
+        var second = fivelet[1].frequency;
+        var third = fivelet[2].frequency;
+        var fourth = fivelet[3].frequency;
+        var fifth = fivelet[4].frequency;
+        
+        if((third > second) && (third > fourth)){
+            //res.write("Timestamp: "+ fivelet[1].timestamp +"\n");
+            if (first < second && fourth > fifth) {
+                return fivelet[2].timestamp;
+            }
         }
         return 0;
     };
-
-    var triplet = [];
+    
+    var fivelet = [];
     var timeDiffs = [];
     var lastPeakTimestamp;
 
-    for (var i = 0; i <= 1000; i++) {
-        var frequency = fs.readFileSync('/sys/devices/ocp.2/helper.14/AIN0');
+	for (var i = 0; i <= 3000; i++) {
+		var frequency = fs.readFileSync('/sys/devices/ocp.2/helper.14/AIN0');
         frequency = parseInt(frequency);
-        console.log("frequency:" + frequency);
-        if ((frequency === 0) || (frequency === 1799)) {
+        //console.log("frequency:"+ frequency+ "\n");
+        if((frequency === 0) || (frequency === 1799) || (frequency === 1)){
             continue;
         }
-        res.write("frequency:" + frequency);
-
+		
+        
         var timestamp = new Date().getTime();
-        var currentElement = {timestamp: timestamp, frequency: frequency};
-
-        triplet.push("currentElement:" + currentElement);
-        triplet = triplet.slice(1, 4);
-        var peakTimestamp = peakFinder(triplet);
-
-        if (peakTimestamp !== 0) {
-            if (!lastPeakTimestamp) {
-                var timeDiff = lastPeakTimestamp - peakTimestamp;
-                timeDiffs.push(timeDiff);
+        var currentElement = {timestamp:timestamp, frequency:frequency};
+        
+        fivelet.unshift(currentElement);
+        fivelet = fivelet.slice(0,5);
+        
+        var peakTimestamp = peakFinder(fivelet);
+        
+        if(peakTimestamp !== 0){
+            if(lastPeakTimestamp){
+                //res.write("Timestamp: "+ peakTimestamp +"\n");
+                var timeDiff = peakTimestamp - lastPeakTimestamp;
+                
+                timeDiffs.push(timeDiff/1000*60);
             }
             lastPeakTimestamp = peakTimestamp;
         }
-        measurements.push(currentElement);
-    }
+		measurements.push(currentElement);
+	}
     res.write(timeDiffs.toString());
-    res.end("end");
+	res.end("end");
 }).listen(8080);
